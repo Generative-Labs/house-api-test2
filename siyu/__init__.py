54d37d27d7e19f5b309c25a5e8c04e0cda3ed1a8
Tianjie Sun <tianjiesun@Tianjies-MacBook-Air.local>
2021-07-11 16:26:33 -0700

    tianjie/dev1 Move db conn string to env

--- siyu/__init__.py
+++ siyu/__init__.py
@@ -9,7 +9,7 @@ application = app = Flask(__name__)
 cors = CORS(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://field:fieldyc2020@database-2.ccrl6jgkfooz.us-west-1.rds.amazonaws.com:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_CONN']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_SECRET_KEY'] = b'+S\x8b>\x07\xa3\x14|ny\xb2u'
